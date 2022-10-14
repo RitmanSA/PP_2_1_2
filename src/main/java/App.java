70e7a9b22f2d5143a53c0e22ef449b98cfcca196
RitmanSA <rsas2001@gmail.com>
2022-10-14 17:08:18 +0300

    Class "Cat" added

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,15 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld newBean = (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat FirstCat = (Cat) applicationContext.getBean("cat");
+        Cat SecondCat = (Cat) applicationContext.getBean("cat");
+
+        System.out.println("beans have the same adress: " + (bean == newBean));
+        System.out.println("Cats have the same adress: " + (FirstCat == SecondCat));
+
+
     }
 }
